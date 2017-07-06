72f6b0f651f3e45b5edb7a67fef3217313739d3e
jasonkclee <jasonkjl72@gmail.com>
2017-07-06 14:15:01 -0400

    Created MySurfaceView

---
+++ app/src/main/java/com/example/eagletest/eaglething/MySurfaceView.java
@@ -0,0 +1,70 @@
+package com.example.eagletest.eaglething;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by Jason Lee on 2017-07-06.
+ */
+
+public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+    public MySurfaceView(Context context) {
+        super(context);
+        SurfaceHolder surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        //canvas.drawCircle();
+        //canvas.drawColor(Color.BLUE);
+        //canvas.drawBitmap(myChar, 10, 10, null);
+    }
+
+    /**
+     * This is called immediately after the surface is first created.
+     * Implementations of this should start up whatever rendering code
+     * they desire.  Note that only one thread can ever draw into
+     * a {@link Surface}, so you should not draw into the Surface here
+     * if your normal rendering will be in another thread.
+     *
+     * @param holder The SurfaceHolder whose surface is being created.
+     */
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    /**
+     * This is called immediately after any structural changes (format or
+     * size) have been made to the surface.  You should at this point update
+     * the imagery in the surface.  This method is always called at least
+     * once, after {@link #surfaceCreated}.
+     *
+     * @param holder The SurfaceHolder whose surface has changed.
+     * @param format The new PixelFormat of the surface.
+     * @param width  The new width of the surface.
+     * @param height The new height of the surface.
+     */
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    /**
+     * This is called immediately before a surface is being destroyed. After
+     * returning from this call, you should no longer try to access this
+     * surface.  If you have a rendering thread that directly accesses
+     * the surface, you must ensure that thread is no longer touching the
+     * Surface before returning from this function.
+     *
+     * @param holder The SurfaceHolder whose surface is being destroyed.
+     */
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
