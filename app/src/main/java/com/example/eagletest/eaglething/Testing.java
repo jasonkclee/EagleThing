1388add7ae35769c76838d647efdee10228d64ee
jasonkclee <jasonkjl72@gmail.com>
2017-07-06 13:30:58 -0400

    Changed Testing file, test 2

--- app/src/main/java/com/example/eagletest/eaglething/Testing.java
+++ app/src/main/java/com/example/eagletest/eaglething/Testing.java
@@ -1,12 +1,15 @@
 package com.example.eagletest.eaglething;
 
+import android.support.v7.app.AlertDialog;
+
 /**
  * Created by Jason Lee on 2017-07-06.
  */
 
 public class Testing {
     public Testing(){
-        /*
+
+        /* Change this into some context thing?
         AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
         dlgAlert.setMessage("This is an alert with no consequence");
         dlgAlert.setTitle("App Title");
